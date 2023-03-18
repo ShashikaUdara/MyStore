cbca848e2b2eb63da0c44ae0ff0579ff7bcaa1e9
Udara Abeyrathne <udara.abeyrathne@ivedha.com>
2023-03-18 13:40:05 +0530

    repo name updated

--- mystore/urls.py
+++ mystore/urls.py
@@ -1,4 +1,4 @@
-"""storefront URL Configuration
+"""mystore URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
